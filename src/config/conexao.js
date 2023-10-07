3305bfec336438a7215313ecb00125a8483a9d27
Victor Fernando <adm.victorfernando@gmail.com>
2023-10-07 13:59:29 -0300

    alterando arquivo conexao.js

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -1,12 +1,23 @@
+
 const { Pool } = require('pg')
 require('dotenv').config()
-
+/*
 const pool = new Pool({
   user: process.env.user,
   host: process.env.host,
   database: process.env.database,
   password: process.env.password,
   port: process.env.port,
+})
+
+module.exports = pool
+*/
+const pool = new Pool ({
+  user: "postgres",
+  host: "localhost",
+  database: "dindin",
+	password: "0000",
+	port: 5432
 })
 
 module.exports = pool
